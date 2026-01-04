b168e37ccac57eadc174971d65a0a09f82171aa9
iamthefalsh <zerapagamentos@gmail.com>
2026-01-03 23:44:52 -0300

    Delete api directory

--- api/plugin/disconnect.js
+++
@@ -1,9 +0,0 @@
-import { loadDB, saveDB } from "../_db.js";
-
-export default function handler(req, res) {
-  const token = req.headers.authorization?.replace("Bearer ", "");
-  const db = loadDB();
-  delete db.activeProjectBySession[token];
-  saveDB(db);
-  res.json({ ok: true });
-}
